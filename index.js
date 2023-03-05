38934c40af1fcaf28aa1a8bc224d1c7b5239016a
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-03-05 15:22:26 +0800

    Finish 4.1-4.2

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./app')
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const PORT = config.PORT || 3003
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
