e81a3ec6043c3ef215564cbfbf1ce2c799ac90a4
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-03-14 23:36:49 +0800

    Finish 4.18

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const logingRouter = require('./controllers/login')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', logingRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
