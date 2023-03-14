6a92da28706bc748430407f519ac540c7332d55a
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-03-14 23:21:34 +0800

    Part of Part4.17

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,11 @@ const userRouter = require('express').Router()
 const User = require('../models/user')
 
 userRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+  })
   response.json(users)
 })
 
--- models/user.js
+++ models/user.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userScheme = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minLength: 3,
-  },
-  name: String,
-  passwordHash: String,
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Blog',
     },
   ],
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  name: String,
+  passwordHash: String,
 })
 
 userScheme.plugin(uniqueValidator)
