440ddd9393d1589d491589061b96c857f804730a
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-11 03:27:34 +0800

    refactor errorHanlder

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,14 +17,16 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({ error: 'invalid token' })
-  } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({ error: 'token expired' })
+  switch (error.name) {
+    case 'CastError':
+      return response.status(400).send({ error: 'malformatted id' })
+    case 'ValidationError':
+      return response.status(400).json({ error: error.message })
+    case 'JsonWebTokenError':
+      return response.status(401).json({ error: 'invalid token' })
+    case 'TokenExpiredError':
+      return response.status(401).json({ error: 'token expired' })
+    default:
   }
   next(error)
 }
